ff4a72aa2f63fba79df2f2629934edcaf2880976
Luis Torres <luisdeavila010@gmail.com>
2022-06-01 10:42:07 -0500

    Obtención de la data

--- DataSet_Covid_Colombia.py
+++ DataSet_Covid_Colombia.py
@@ -5,4 +5,11 @@ Created on Wed Jun  1 10:33:27 2022
 @author: Luis Torres
 """
 
+import pandas as pd
+
+# Obtener la data
+
+url = 'Casos_positivos_de_COVID-19_en_Colombia.csv'
+data = pd.read_csv(url)[:10000]
+
 

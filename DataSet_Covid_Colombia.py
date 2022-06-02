9e4c95d3b44521aa8045dc9d5a2ca423b72e5675
Luis Torres <luisdeavila010@gmail.com>
2022-06-01 22:20:35 -0500

    Consultas de la 1 - 5

--- DataSet_Covid_Colombia.py
+++ DataSet_Covid_Colombia.py
@@ -6,6 +6,8 @@ Created on Wed Jun  1 10:33:27 2022
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # Obtener la data
 
@@ -24,5 +26,38 @@ data.drop('Unidad de medida de edad', axis = 1, inplace=True)
 data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
 data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
 data.drop('ID de caso', axis = 1, inplace=True)
+
+
+# 1.Número de casos de Contagiados en el País
+
+numero_casos = data.shape[0]
+print(f'El número de casos de contagiados en el pais es de: {numero_casos}')
+
+
+# 2.Número de Municipios Afectados
+
+numero_municipios = data['Nombre municipio'].value_counts().count()
+print(f'El número de municipios afectados es: {numero_municipios}')
+
+
+# 3.Liste los municipios afectados (sin repetirlos)
+
+municipios_afectados = data['Nombre municipio'].value_counts()
+print(f'Municipios afectados: {municipios_afectados}')
+
+
+# 4.Número de personas que se encuentran en atención en casa
+
+data.loc[data['Ubicación del caso'] == 'casa'] = 'Leve'
+data.loc[data['Ubicación del caso'] == 'CASA'] = 'Leve'
+numero_atencion_casa = data[data['Ubicación del caso'] == 'Casa'].shape[0]
+print(f'El numero de personas con atención en casa es de: {numero_atencion_casa}')
+
+
+# 5.Número de personas que se encuentran recuperados
+
+numero_recuperados = data[data['Recuperado'] == 'Recuperado'].shape[0]
+print(f'El numero de personas recuperadas es de: {numero_recuperados}')
+
 
 
